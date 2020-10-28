1224124ae22516a92217e6710616a27f39b720df
Peter Pan <petarzivkovic85@gmail.com>
2020-10-28 16:06:43 -0700

    new functioning quiz created using DOM manipulation

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        title: 'What is the capital of US?',
+        choices: ['Los Angeles', 'Washington D.C.', 'New York', 'Seattle'],
+        answer: 'Washington D.C.'
+    },
+    {
+        title: 'What is the capital of Canada?',
+        choices: ['Ottawa', 'Montreal', 'Victoria', 'Quebec'],
+        answer: 'Ottawa'
+    },
+    {
+        title: 'What is the capital of Germany?',
+        choices: ['Berlin', 'Bon', 'Dortmund', 'Hamburgh'],
+        answer: 'Berlin'
+    },
+    {
+        title: 'What is the capital of France?',
+        choices: ['Paris', 'Strasbourg', 'Tolouse', 'Nantes'],
+        answer: 'Paris'
+    },
+    {
+        title: 'What is the capital of Italy?',
+        choices: ['Rome', 'Naples', 'Torino', 'Genoa'],
+        answer: 'Rome'
+    }
+];
